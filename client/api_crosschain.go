c5e2e2339f9a83360e002c6915f2beaa65257b0a
phenix3443 <phenix3443@gmail.com>
2026-05-21 15:04:14 +0800

    fix: disable removed crosschain helpers

--- client/api_crosschain.go
+++ client/api_crosschain.go
@@ -3,16 +3,11 @@ package client
 import (
 	"context"
 	"errors"
-	"strings"
 
 	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/common"
 	oracletypes "github.com/cosmos/cosmos-sdk/x/oracle/types"
-	bsccommon "github.com/mocachain/moca-go-sdk/common"
 	gnfdSdkTypes "github.com/mocachain/moca/v2/sdk/types"
-	mocadTypes "github.com/mocachain/moca/v2/types"
 )
 
 var errCrossChainFeatureRemoved = errors.New("cross-chain chain module APIs are not supported by github.com/mocachain/moca/v2 main")
@@ -29,39 +24,8 @@ type ICrossChainClient interface {
 	MirrorObject(ctx context.Context, destChainId sdk.ChainID, objectId math.Uint, bucketName, objectName string, txOption gnfdSdkTypes.TxOption) (*sdk.TxResponse, error)
 }
 
-// TransferOut uses the ERC20 precompile because the old cross-chain module path was removed from moca main.
 func (c *Client) TransferOut(ctx context.Context, toAddress string, amount math.Int, txOption gnfdSdkTypes.TxOption) (*sdk.TxResponse, error) {
-	txHash, err := c.sendTransferOutEvmTx(ctx, toAddress, amount)
-	if err != nil {
-		return nil, err
-	}
-	return &sdk.TxResponse{TxHash: txHash}, nil
-}
-
-func (c *Client) sendTransferOutEvmTx(ctx context.Context, to string, amount math.Int) (string, error) {
-	nonce, err := c.chainClient.GetNonce(context.Background())
-	if err != nil {
-		return "", err
-	}
-	chainID, err := c.evmClient.ChainID(ctx)
-	if err != nil {
-		return "", err
-	}
-	txOpts, err := CreateTxOpts(context.Background(), c.evmClient, c.privateKey, chainID, DefaultGasLimit, nonce)
-	if err != nil {
-		return "", err
-	}
-
-	parsedABI, err := abi.JSON(strings.NewReader(bsccommon.TokenABI))
-	if err != nil {
-		return "", err
-	}
-	contract := bindContract(common.HexToAddress(mocadTypes.Erc20Address), parsedABI, c.evmClient)
-	tx, err := contract.Transact(txOpts, "transferOut", common.HexToAddress(to), amount.BigInt())
-	if err != nil {
-		return "", err
-	}
-	return tx.Hash().String(), nil
+	return nil, errCrossChainFeatureRemoved
 }
 
 func (c *Client) Claims(ctx context.Context, srcChainId, destChainId uint32, sequence uint64,
