a6b0c48897066998a5c95a269f505fba351e39a7
phenix3443 <phenix3443@gmail.com>
2026-06-03 10:50:56 +0800

    fix(feegrant): use bech32 addresses

--- client/api_feegrant.go
+++ client/api_feegrant.go
@@ -7,7 +7,9 @@ import (
 	"cosmossdk.io/math"
 	"cosmossdk.io/x/feegrant"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	cmdcfg "github.com/mocachain/moca/v2/cmd/config"
 	gnfdsdktypes "github.com/mocachain/moca/v2/sdk/types"
+	mocautils "github.com/mocachain/moca/v2/utils"
 )
 
 type IFeeGrantClient interface {
@@ -24,7 +26,7 @@ type IFeeGrantClient interface {
 
 // GrantBasicAllowance grants the grantee the BasicAllowance with specified amount and expiration.
 func (c *Client) GrantBasicAllowance(ctx context.Context, granteeAddr string, feeAllowanceAmount math.Int, expiration *time.Time, txOption gnfdsdktypes.TxOption) (string, error) {
-	grantee, err := sdk.AccAddressFromHexUnsafe(granteeAddr)
+	grantee, granteeAddrBech32, err := normalizeFeeGrantAccountAddress(granteeAddr)
 	if err != nil {
 		return "", err
 	}
@@ -37,16 +39,26 @@ func (c *Client) GrantBasicAllowance(ctx context.Context, granteeAddr string, fe
 	if err != nil {
 		return "", err
 	}
+	msg.Grantee = granteeAddrBech32
+	msg.Granter, err = feeGrantAddressCodec.BytesToString(c.defaultAccount.GetAddress())
+	if err != nil {
+		return "", err
+	}
 	return c.sendTxn(ctx, msg, &txOption)
 }
 
 // GrantAllowance provides a generic way to grant different types of allowance(BasicAllowance, PeriodicAllowance, AllowedMsgAllowance), the user needs to construct the desired type of allowance
 func (c *Client) GrantAllowance(ctx context.Context, granteeAddr string, allowance feegrant.FeeAllowanceI, txOption gnfdsdktypes.TxOption) (string, error) {
-	grantee, err := sdk.AccAddressFromHexUnsafe(granteeAddr)
+	grantee, granteeAddrBech32, err := normalizeFeeGrantAccountAddress(granteeAddr)
 	if err != nil {
 		return "", err
 	}
 	msg, err := feegrant.NewMsgGrantAllowance(allowance, c.defaultAccount.GetAddress(), grantee)
+	if err != nil {
+		return "", err
+	}
+	msg.Grantee = granteeAddrBech32
+	msg.Granter, err = feeGrantAddressCodec.BytesToString(c.defaultAccount.GetAddress())
 	if err != nil {
 		return "", err
 	}
@@ -59,11 +71,13 @@ func (c *Client) GrantAllowance(ctx context.Context, granteeAddr string, allowan
 
 // RevokeAllowance revokes allowance on a grantee by the granter
 func (c *Client) RevokeAllowance(ctx context.Context, granteeAddr string, txOption gnfdsdktypes.TxOption) (string, error) {
-	grantee, err := sdk.AccAddressFromHexUnsafe(granteeAddr)
+	grantee, granteeAddrBech32, err := normalizeFeeGrantAccountAddress(granteeAddr)
 	if err != nil {
 		return "", err
 	}
 	msg := feegrant.NewMsgRevokeAllowance(c.defaultAccount.GetAddress(), grantee)
+	msg.Grantee = granteeAddrBech32
+	msg.Granter, err = feeGrantAddressCodec.BytesToString(c.defaultAccount.GetAddress())
 	if err != nil {
 		return "", err
 	}
@@ -88,19 +102,19 @@ func (c *Client) QueryBasicAllowance(ctx context.Context, granterAddr, granteeAd
 }
 
 func (c *Client) QueryAllowance(ctx context.Context, granterAddr, granteeAddr string) (*feegrant.Grant, error) {
-	_, err := sdk.AccAddressFromHexUnsafe(granterAddr)
+	_, granterAddrBech32, err := normalizeFeeGrantAccountAddress(granterAddr)
 	if err != nil {
 		return nil, err
 	}
-	_, err = sdk.AccAddressFromHexUnsafe(granteeAddr)
+	_, granteeAddrBech32, err := normalizeFeeGrantAccountAddress(granteeAddr)
 	if err != nil {
 		return nil, err
 	}
 	req := &feegrant.QueryAllowanceRequest{
-		Granter: granterAddr,
-		Grantee: granteeAddr,
+		Granter: granterAddrBech32,
+		Grantee: granteeAddrBech32,
 	}
-	response, err := c.chainClient.FeegrantQueryClient.Allowance(ctx, req)
+	response, err := c.chainClient.Allowance(ctx, req)
 	if err != nil {
 		return nil, err
 	}
@@ -108,14 +122,14 @@ func (c *Client) QueryAllowance(ctx context.Context, granterAddr, granteeAddr st
 }
 
 func (c *Client) QueryAllowances(ctx context.Context, granteeAddr string) ([]*feegrant.Grant, error) {
-	_, err := sdk.AccAddressFromHexUnsafe(granteeAddr)
+	_, granteeAddrBech32, err := normalizeFeeGrantAccountAddress(granteeAddr)
 	if err != nil {
 		return nil, err
 	}
 	req := &feegrant.QueryAllowancesRequest{
-		Grantee: granteeAddr,
+		Grantee: granteeAddrBech32,
 	}
-	response, err := c.chainClient.FeegrantQueryClient.Allowances(ctx, req)
+	response, err := c.chainClient.Allowances(ctx, req)
 	if err != nil {
 		return nil, err
 	}
@@ -123,16 +137,34 @@ func (c *Client) QueryAllowances(ctx context.Context, granteeAddr string) ([]*fe
 }
 
 func (c *Client) QueryGranterAllowances(ctx context.Context, granterAddr string) ([]*feegrant.Grant, error) {
-	_, err := sdk.AccAddressFromHexUnsafe(granterAddr)
+	_, granterAddrBech32, err := normalizeFeeGrantAccountAddress(granterAddr)
 	if err != nil {
 		return nil, err
 	}
 	req := &feegrant.QueryAllowancesByGranterRequest{
-		Granter: granterAddr,
+		Granter: granterAddrBech32,
 	}
-	response, err := c.chainClient.FeegrantQueryClient.AllowancesByGranter(ctx, req)
+	response, err := c.chainClient.AllowancesByGranter(ctx, req)
 	if err != nil {
 		return nil, err
 	}
 	return response.Allowances, nil
+}
+
+var feeGrantAddressCodec = cmdcfg.NewMultiPrefixBech32AccCodec()
+
+func normalizeFeeGrantAccountAddress(addr string) (sdk.AccAddress, string, error) {
+	acc, err := sdk.AccAddressFromHexUnsafe(addr)
+	if err != nil {
+		acc, err = mocautils.GetMocaAddressFromBech32(addr)
+		if err != nil {
+			return nil, "", err
+		}
+	}
+
+	bech32Addr, err := feeGrantAddressCodec.BytesToString(acc)
+	if err != nil {
+		return nil, "", err
+	}
+	return acc, bech32Addr, nil
 }
